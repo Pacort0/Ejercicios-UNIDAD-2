34fe438fbe0b50e1b2e440364c2287b5ec7072c0
siu <francisco.rodriguez@iesnervion.es>
2023-11-14 12:49:16 +0100

    first commit

---
+++ Ejercicio01/Ejercicio01.py
@@ -0,0 +1,22 @@
+from multiprocessing import *
+from funciones import *
+
+if __name__ == '__main__':
+
+    p1 = Process(target=sumaHastaMax, args=[7])
+    p2 = Process(target=sumaHastaMax, args=[8])
+    p3 = Process(target=sumaHastaMax, args=[0])
+    p4 = Process(target=sumaHastaMax, args=[3])
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    
+    print("Procesos acabaos")
+
---
+++ Ejercicio01/funciones.py
@@ -0,0 +1,8 @@
+def sumaHastaMax(numeroMax):
+    suma = 0
+    rango = range(numeroMax+1)
+
+    for numero in rango:
+        suma+=numero
+
+    print (suma)
