573a0ae732953f58d72319411d2174ec8c23dd6a
siu <francisco.rodriguez@iesnervion.es>
2023-12-04 14:38:44 +0100

    aaaaaaaaaa

--- PARTE-2/Ejercicio03/Ejercicio03.py
+++ PARTE-2/Ejercicio03/Ejercicio03.py
@@ -1,7 +1,28 @@
 from multiprocessing import *
-from random import *
+import random
 
-def procesoGeneraNotas(cantidad):
+def procesoGeneraNotas(cantidad, fichero):
+    with open(fichero, "w") as archivo:
+        for i in range(cantidad):
+            num = round(random.uniform(0,10), 2)
+            archivo.write(f"{num}\n")
 
-    for nota in cantidad:
-        nota = round(random.uniform(1.00,10.01))
+def mediaNota(fichero, nombreAlumno):
+    with open(fichero, "r") as archivo:
+        suma = 0
+        for linea in archivo.readlines():
+            nota = float(linea)
+            suma += nota
+        media = suma/6
+    with open("medias.txt", "a") as medias:
+        medias.write(f"{media}{nombreAlumno}\n")
+
+if __name__ == "__main__":
+    for i in range(10):
+        proceso1 = Process(target=procesoGeneraNotas, args=(10, f"Alumno{i+1}.txt"))
+        proceso1.start()
+    
+    for i in range(10):
+        proceso2 = Process(target=mediaNota, args=(f"Alumno{i+1}.txt", f"Alumno{i+1}"))
+        proceso2.start()
+    
